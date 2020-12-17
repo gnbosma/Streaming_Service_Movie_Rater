97c6be1b7ac7e473124e82f077c776bf466da371
Dylan Clark-Boucher <clarkbou@umich.edu>
2020-12-17 17:58:39 -0500

    update shiny

--- Movie_Rater_Shiny/server.R
+++ Movie_Rater_Shiny/server.R
@@ -8,18 +8,53 @@
 #
 
 library(shiny)
+library(shinythemes)
 
-# Define server logic required to draw a histogram
+#Choose the location of data in your files. Too big to go on github
+#load("C:\Users\dc48b\Dropbox (University of Michigan)\BIOS 625 - Big Data Computing\FinalProject\data\data_clean.rda")
+
+titles <- dat %>% 
+    distinct(Title,.keep_all = T) %>% 
+    mutate(RottenTomatoes = RottenTomatoes * 100) %>% 
+    filter(!is.na(IMDb),!is.na(RottenTomatoes))
+
+inRange <- function(x,range){
+    x <- as.numeric(x)
+    range <- as.numeric(range)
+    return(x >= range[1] & x <= range[2])
+}
+
+
 shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    output$histplot <- renderPlot({
+        
+        #Filter according to inputs
+        right_service <- titles %>% 
+            select(all_of(input$service)) %>% 
+            rowSums() %>% as.logical()
+            
+         plot_data <- titles %>% 
+             filter(right_service,
+                    inRange(RottenTomatoes, input$tscore),
+                    inRange(Year,input$yr),
+                    Age %in% input$ages
+                    )
+        
+        #Make sure bin count is fine
+        nbins <- input$bins
+        if(nbins > nrow(plot_data)){
+            nbins <- nrow(plot_data)
+        }
+        
+        #Create histogram
+        ggplot(plot_data) + 
+            geom_histogram(mapping = aes(x = IMDb), 
+                           bins = nbins, color = "white",
+                           fill = input$color) +
+            labs(y = "Count", x = "Movie Rating") + 
+            xlim(0,10) +
+            theme_minimal()
 
     })
 
--- Movie_Rater_Shiny/ui.R
+++ Movie_Rater_Shiny/ui.R
@@ -8,26 +8,60 @@
 #
 
 library(shiny)
-
+library(shinythemes)
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI(fluidPage(theme = shinytheme("slate"),
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Streaming Service Movie Rater"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
 
+        sidebarPanel(
+            checkboxGroupInput("service", label = "Streaming service", 
+                           choices = list("Disney+" = "Disney+", 
+                                          "Hulu" = "Hulu", 
+                                          "Netflix" = "Netflix",
+                                          "PrimeVideo" = "PrimeVideo"),
+                           selected = c("Disney+","Hulu","Netflix",
+                                        "PrimeVideo")
+                           ),
+            
+            sliderInput("tscore", label = "Tomato Score", min = 0, 
+                        max = 100, value = c(0,100)),
+            
+            checkboxGroupInput("ages", label = "Age Group", 
+                        choices = list("All" = "all", "7+" = "7+", 
+                                       "13+" = "13+", "16+" = "16+",
+                                       "18+" = "18+"), 
+                        selected = c("All","7+","13+","16+","18+")),
+            
+            sliderInput("yr",label = "Release year",
+                        min = 1901,
+                        max = 2020,
+                        value = c(1901,2020),
+                        sep = ""),
+            
+            sliderInput("bins",
+                          label = "Number of bins",
+                          min = 5,
+                          max = 40,
+                          value = 20),
+            
+            selectInput("color", label = "Color choice", 
+                        choices = list("Blue" = "slateblue2", 
+                                       "Red" = "firebrick3", 
+                                       "Green" = "seagreen4",
+                                       "Orange" = "sienna2",
+                                       "Purple" = "orchid4"), 
+                        selected = "Blue")
+            
+        ),
+        
         # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
+        mainPanel(tags$h2("IMBd Popular Score"),
+            plotOutput("histplot")
         )
     )
 ))
